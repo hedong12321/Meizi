6af3d26b71d7f6fd4b664b96bd82deb04a7cd4ed
hed <comeonhd@sina.com>
2017-08-18 16:46:28 +0800

    注释掉无用代码，添加备注

--- app/src/main/java/com/spark/meizi/about/AboutActivity.java
+++ app/src/main/java/com/spark/meizi/about/AboutActivity.java
@@ -19,7 +19,30 @@ public class AboutActivity extends BaseActivity {
         super.initSubViews(view);
         setSupportActionBar(tbAbout);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // 对android中ActionBar中setDisplayHomeAsUpEnabled和setHomeButtonEnabled和setDisplayShowHomeEnabled方法的理解
+
+        /*
+        setHomeButtonEnabled这个小于4.0版本的默认值为true的。
+        但是在4.0及其以上是false，该方法的作用：决定左上角的图标是否可以点击。没有向左的小图标。 true 图标可以点击  false 不可以点击。
+
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        // 给左上角图标的左边加上一个返回的图标 。对应ActionBar.DISPLAY_HOME_AS_UP
+
+        actionBar.setDisplayShowHomeEnabled(true)
+        //使左上角图标是否显示，如果设成false，则没有程序图标，仅仅就个标题，否则，显示应用程序图标，对应id为 android.R.id.home，对应ActionBar.DISPLAY_SHOW_HOME
+
+        actionBar.setDisplayShowCustomEnabled(true)
+        // 使自定义的普通View能在title栏显示，即actionBar.setCustomView能起作用，对应 ActionBar.DISPLAY_SHOW_CUSTOM
+
+        actionBar.setDisplayShowTitleEnabled(true)
+        //对应ActionBar.DISPLAY_SHOW_TITLE。
+
+        其中setHomeButtonEnabled和setDisplayShowHomeEnabled共同起作用，
+        如果setHomeButtonEnabled设成false，
+        即使setDisplayShowHomeEnabled设成true，图标也不能点击
+         */
     }
 
     @Override
